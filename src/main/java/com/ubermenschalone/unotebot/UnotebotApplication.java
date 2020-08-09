25f25bd122b88e905d175e80f4dd89f66e4c6157
unknown <ubermenschalone@gmail.com>
2020-08-09 16:14:46 +0500

    Initial commit

---
+++ src/main/java/com/ubermenschalone/unotebot/UnotebotApplication.java
@@ -0,0 +1,13 @@
+package com.ubermenschalone.unotebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnotebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnotebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubermenschalone/unotebot/UnotebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubermenschalone.unotebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnotebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
